bf3892b1a0278e5dbeca37770f959d6722d5f0d1
Feku <feikrpg@yandex.ru>
2025-07-07 18:06:01 +0300

    Обновление скрипта получения книги
    Добавил скрипт для получения популярных сейчас книг. Мелкие изменение в получении книги по названию.

--- client/scripts/books.js
+++ client/scripts/books.js
@@ -1,12 +1,12 @@
 //Получение книги через название
-async function getBookByTitle(title) {
+async function getFromGoogle(title) {
     try {
-        const response = await fetch(
+        const responseGoogle = await fetch(
             `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(title)}&langRestrict=ru`
         )
-        const data = await response.json()
-        if (data) {
-            const book = data.items[0].volumeInfo
+        const dataGoogle = await responseGoogle.json()
+        if (dataGoogle) {
+            const book = dataGoogle.items[0].volumeInfo
 
             return {
                 title: book.title,
@@ -14,9 +14,9 @@ async function getBookByTitle(title) {
                 year: book.publishedDate,
                 description: book.description || 'Описание отсутствует',
                 isbn: book.industryIdentifiers?.find(id => id.type === 'ISBN_10')?.identifier || 'Не найден',
-                id: data.items[0].id,
-                image: `https://books.google.com/books/content?id=${data.items[0].id}&printsec=frontcover&img=1&zoom=2`,
-                publisher: book.publisher,
+                id: dataGoogle.items[0].id,
+                image: `https://books.google.com/books/content?id=${dataGoogle.items[0].id}&printsec=frontcover&img=1&zoom=2`,
+                publisher: book.publisher || "Издатель неизвестен",
                 pageCount: book.pageCount || "Количество страниц не указано"
             }
         } else {
@@ -28,6 +28,17 @@ async function getBookByTitle(title) {
     }
 }
 
-// getBookByTitle("аист марабу").then(book => {
-//   console.log(book);
-// });
+getPopularBooks('fiction').then( books => {
+    console.log(books)
+})
+
+//Получение популярных книг 
+
+async function getPopularBooks(category = "fiction") {
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=subject:${category}&orderBy=newest&maxResults=20&langRestrict=ru&key=AIzaSyCLID0PjuG3JwDLr33ZWwj-bQUiAZ38kRY`
+  );
+  const data = await response.json();
+  const books = data.items.volumeInfo
+  
+}
