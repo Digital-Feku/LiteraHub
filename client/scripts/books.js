8b60ccb2e7ecc9803118797b7de8212ae6f7f6d1
Feku <feikrpg@yandex.ru>
2025-07-11 19:27:58 +0300

    Обновление скриптов для получения книг
    Оптимизировал скрипты и добавил проверку ошибок. Обновил файл README

--- client/scripts/books.js
+++ client/scripts/books.js
@@ -1,72 +1,54 @@
 //Получение книги через название
-async function getFromGoogle(title) {
+async function getBookByTitle(title) {
     try {
-        const responseGoogle = await fetch(
-            `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(title)}+lang:ru&langRestrict=ru`
-        )
-        const dataGoogle = await responseGoogle.json()
-        if (dataGoogle) {
-            const book = dataGoogle.items[0].volumeInfo
-
-            return {
-                title: book.title,
-                author: book.authors[0],
-                year: book.publishedDate,
-                description: book.description || 'Описание отсутствует',
-                isbn: book.industryIdentifiers?.find(id => id.type === 'ISBN_10')?.identifier || 'Не найден',
-                id: dataGoogle.items[0].id,
-                image: `https://books.google.com/books/content?id=${dataGoogle.items[0].id}&printsec=frontcover&img=1&zoom=2`,
-                publisher: book.publisher || "Издатель неизвестен",
-                pageCount: book.pageCount || "Количество страниц не указано"
-            }
-        } else {
-            return 'Ничего не найдено'
-        }
+        const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(title)}`
+      );
+    const data = await response.json();
+     const book = data.items[0].volumeInfo
+sendResponse({
+    book: {
+        title: book.title,
+        description: book.description || 'Описание отсутствует',
+        pages: book.pageCount ||  'Не указано',
+        author: book.authors || 'Автор неизвестен',
+        publisher: book.publisher || 'Неизвестно',
+        publishedDate: book.publishedDate ? String(book.publishedDate).slice(0, 4) : 'Неизвестен',
+        language: book.language || 'Не указан',
+        image: `https://books.google.com/books/content?id=${data.items[0].id}&printsec=frontcover&img=1&zoom=3`
+     }
+    })
     } catch (error) {
-        console.log('Ошибка:', error )
-        return null
+        console.log('Ошибка получения книги:', error)
+        return null;
+    }
+
+}
+
+async function getRecommendations(count) {
+    try {
+        const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=${count}`
+      );
+      const data = await response.json();
+      const recommendations = data.items.slice(0, count).map(item => {
+        const book = data.items[i].volumeInfo    
+        return {
+        title: book.title,
+        description: book.description || 'Описание отсутствует',
+        pages: book.pageCount ||  'Не указано',
+        author: book.authors || 'Автор неизвестен',
+        publisher: book.publisher || 'Неизвестно',
+        publishedDate: book.publishedDate ? String(book.publishedDate).slice(0, 4) : 'Неизвестен',
+        language: book.language || 'Не указан',
+        image: `https://books.google.com/books/content?id=${data.items[0].id}&printsec=frontcover&img=1&zoom=3`
+
+     }
+      })
+      sendResponse({ recommendations });
+    } catch (error) {
+        console.log('Ошибка получения рекомендаций:', error)
     }
 }
 
-//Получение популярных книг 
 
-async function getPopularBooks(count) {
-    try {
-  const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&langRestrict=en&keyes&key=AIzaSyCLID0PjuG3JwDLr33ZWwj-bQUiAZ38kRY`
-  );
-
-        if (!response.ok) {
-        throw new Error(`Ошибка запроса: ${response.status}`);
-        }
-
-    const data = await response.json();
-
-    if (!data.items || data.items.length === 0) {
-        console.log("Книги не найдены");
-        return;
-        }
-for (let i = 0; i < count; i++) {
-
-     const book = await data.items[i].volumeInfo 
-        const books = {
-        id: data.items[i].id,
-        title: book.title,
-        authors: book.authors || ["Автор неизвестен"],
-      };
-      console.log(books)
-
-}
-   
-
-} catch (error) {
-    console.error("Ошибка при получении книг:", error);
-    return [];
-  }
-}
-
-// getFromGoogle('fight club').then(book => {
-//     console.log(book)
-// })
-
-getPopularBooks(3)
