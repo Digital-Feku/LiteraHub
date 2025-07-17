7d6a6df23aec740d7c7f8454ab790ac78946d68c
Feku <feikrpg@yandex.ru>
2025-07-17 14:01:45 +0300

    Вывод книг в рекомендации на главной странице
    Книги теперь корректно выводятся на главную страницу в рекомендациях.

--- client/scripts/main.mjs
+++ client/scripts/main.mjs
@@ -10,6 +10,9 @@ async function init() {
     
     try {
         const popularBooksImages = document.querySelectorAll('.popular-image')
+        const popularTitle = document.querySelectorAll('.popular-title')
+        const popularAuthor = document.querySelectorAll('.popular-author')
+        const popularDescription = document.querySelectorAll('.popular-description')
         const recommendations = await getRecommendations(3);
 
         if (recommendations.length === 0) {
@@ -18,6 +21,20 @@ async function init() {
         }
         for (let i = 0; i < 3; i++) {
         popularBooksImages[i].style.background = `url('${recommendations[i].image}')`
+        popularBooksImages[i].style.backgroundSize = 'cover'; 
+        popularBooksImages[i].style.backgroundPosition = 'center';
+        popularBooksImages[i].style.backgroundRepeat = 'no-repeat';
+
+        if (recommendations[i].title.length > 12) {
+            
+            popularTitle[i].style.fontSize = '24px'   
+            popularTitle[i].innerHTML = recommendations[i].title
+
+        } else { popularTitle[i].innerHTML = recommendations[i].title }
+
+        popularAuthor[i].innerHTML = recommendations[i].author
+
+        popularDescription[i].innerHTML = recommendations[i].description.substring(0, 150) + '…'
         }
 
         
